473297a8a4fa66d952475ef42636f4019c9f7ada
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-07-21 23:05:02 -0300

    erros arrumados

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -13,36 +13,55 @@ export default function Register() {
   const [jobTitle, setJoTitle] = useState('');
   let [showErroNameEmpty, setErrorNameEmpty] = useState(false);
   let [showErroEmailInvalid, setErrorEmailInvalid] = useState(false);
-  let [showErroEmptyFields, setErrorEmptyFields] = useState(false);
   let [showErroPassword, setErrorPassword] = useState(false);
 
   //quando der tudo certo vai guardar as informações do usuário lá no firebase, e quando não ser, vai pegar aquele errinhoq ue ao próprio firebase retorna e verificar
-  //se o erro for igual a o erro do firebase vai chamar a função de erro caso o email seja inválido e mostrar a mensagem personalizada que está abaixo de cada input
+  //se houver algum erro, irá executar a função e mostrar a mensagem personalizada que está abaixo de cada input
+  function validForm() {
+    setErrorNameEmpty(false);
+    setErrorPassword(false);
+    setErrorEmailInvalid(false);
+
+    let isValid = true;
+    if (!username) {
+      setErrorNameEmpty(true);
+      isValid = false;
+    }
+    if (!password) {
+      setErrorPassword(true);
+      isValid = false;
+    }
+    if (!email) {
+      setErrorEmailInvalid("email obrigatório");
+      isValid = false;
+    }
+    if (!(/\S+@\S+\.\S+/.test(email))) {
+      setErrorEmailInvalid('Formato de e-mail inválido');
+    }
+    return isValid;
+  }
+
   const creatUser = (e) => {
     e.preventDefault();
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(email, password)
-      .then(() => {
-        firebase
-          .firestore()
-          .collection('users').add({
-            username,
-            email,
-            jobTitle,
-            userUid: firebase.auth().currentUser.uid,
-          });
-      }).catch((err) => {
-        if (email === '' || username === '' || jobTitle === '' || password === '') {
-          setErrorEmptyFields(!showErroEmptyFields)
-        } else if (username === '') {
-          setErrorNameEmpty(!showErroNameEmpty)
-        } else if (err.code === 'auth/invalid-email') {
-          setErrorEmailInvalid(!showErroEmailInvalid)
-        } else if (err.code === 'auth/weak-password') {
-          setErrorPassword(!showErroPassword)
-        }
-      })
+    const isValid = validForm();
+    if (isValid) {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(() => {
+          firebase
+            .firestore()
+            .collection('users').add({
+              username,
+              email,
+              jobTitle,
+              userUid: firebase.auth().currentUser.uid,
+            });
+        }).catch((err) => {
+          alert(err.message)
+        })
+    };
+
   }
 
   return (
@@ -52,9 +71,9 @@ export default function Register() {
         {showErroNameEmpty && (
           <p>Por favor, preencha seu nome</p>
         )}
-        <Input type='email' name='email' placeholder='email@exemple.com' id='email-register' onChange={(e) => setEmail(e.target.value)} />
+        <Input type='email' required name='email' placeholder='email@exemple.com' id='email-register' onChange={(e) => setEmail(e.target.value)} />
         {showErroEmailInvalid && (
-          <p>E-mail inválido.</p>
+          <p>{showErroEmailInvalid}</p>
         )}
         <Input type='password' name='password' placeholder='senha' id='password-register' onChange={(e) => setPassword(e.target.value)} />
         {showErroPassword && (
@@ -71,9 +90,6 @@ export default function Register() {
         <Button id='btn-cancel' className='button' name='Cancelar' />
         <Button id='btn-confirm' className='button' name='Confirmar' handleClick={creatUser} />
       </div>
-      {showErroEmptyFields && (
-        <p>Todos os campos devem ser preenchidos</p>
-      )}
     </form>
   )
 }
