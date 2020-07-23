82bae67799109a3c6d7ac287bb152cb73c41a0ec
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-07-23 16:10:07 -0300

    add as imgs, css hall, btns de opção, criando json

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Img from '../imagem/Img'
 import './header.css'
-import logo from '../../assets/logo.png'
+import logo from '../../assets/logo-sem-coelho-final.png'
 
 const Header = (props) => (
 	<header className={props.class}>
-		<Img src={logo} alt='logo' class='logo-login'/>
+		<Img src={logo} alt='logo' class='logo-login' />
 	</header>
 );
 
--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import Img from '../imagem/Img';
+import Button from '../button/Button'
+import optionBurger from '../optionMenu/option'
+import './menu.css'
 
-const Menu = (props) => (
-  <div id={props.id} className={props.class}></div>
+const Menu = () => (
+  <section className='menu'>
+    <div className='menu-principal bg-color'>
+      <div className='border-menu'>
+        {optionBurger.map(item => (
+          <div className='divs-option-menu'>
+            <div className='only-option-menu'>
+              <Img src={item.imagem} alt={item.alt} className='img-food' />
+              <p>{item.text}</p>
+              <p>{item.price}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+    <div className='requests bg-color'>
+      <div className='requests-quantity'>
+        <p>PEDIDOS</p>
+      </div>
+      <div className='total'>
+        <span>Total: </span>
+      </div>
+      <Button />
+    </div>
+  </section>
 );
 
 export default Menu;
