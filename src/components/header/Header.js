a46958f8723f7c3502560b1d61cff1397d6130b1
natasha <natasha.gr.costa@gmail.com>
2020-07-23 18:36:11 -0300

    resolução de conflitos

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import Img from '../imagem/Img'
 import './header.css'
-<<<<<<< HEAD
 import logo from '../../assets/logo-final.png'
-=======
-import logo from '../../assets/logo-sem-coelho-final.png'
->>>>>>> 2b5508e323ca2da26162a8f802cb2074ae99161d
 
 const Header = (props) => (
 	<header className={props.class}>
