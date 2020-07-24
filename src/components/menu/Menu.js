3e6adf013c77ad10c81ad75b736d1fafe88ab19a
natasha <natasha.gr.costa@gmail.com>
2020-07-24 20:16:39 -0300

    firebase pegando os dados do database

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,24 +1,49 @@
 import React from 'react';
 import Img from '../imagem/Img';
 import Button from '../button/Button'
-import optionBurger from '../optionMenu/option'
+// import optionBurger from '../optionMenu/option'
 import './menu.css'
+import firebase from '../../configure-firebase'
+
+const breakfast = (e) => {
+  e.preventDefault()
+  firebase
+    .firestore()
+    .collection('breakfast')
+    .get()
+    .then(querySnapshot => {
+      querySnapshot.forEach(doc => console.log(doc.data()))
+    })
+}
+
+const allDayArray = []; 
+const allDay = () => {
+  firebase
+    .firestore()
+    .collection('allday')
+    .get()
+    .then(querySnapshot => {
+      querySnapshot.forEach(doc => allDayArray.push(doc.data()));
+      // return allDayArray
+      // allDayArray.forEach(categories => categories.burger.forEach(burgers => console.log(burgers.name)))
+    })
+}
 
 const Menu = () => (
   <section className='menu'>
     <div className='div-menu'>
       <div className='buttons-options-menu'>
-        <Button name='Matinal' className='option-menu-food' />
-        <Button name='Almoço/Janta' className='option-menu-food' />
+        <Button name='Matinal' className='option-menu-food' handleClick={breakfast} />
+        <Button name='Almoço/Janta' className='option-menu-food' handleClick={allDay} />
       </div>
       <div className='menu-principal bg-color'>
         <div className='border-menu bg-color'>
-          {optionBurger.map(item => (
+          {allDayArray.forEach(item => (
             <div className='divs-option-menu'>
               <div className='only-option-menu'>
-                <Img src={item.imagem} alt={item.alt} className='img-food' />
-                <p>{item.text}</p>
-                <p>{item.price}</p>
+                <Img alt={item.burger.alt} className='img-food' />
+                <p>{item.burger.name}</p>
+                <p>{item.burger.price}</p>
               </div>
             </div>
           ))}
@@ -32,7 +57,7 @@ const Menu = () => (
       <div className='total'>
         <span>Total: </span>
       </div>
-      <Button name='PEDIR'/>
+      <Button name='PEDIR' />
     </div>
   </section>
 );
