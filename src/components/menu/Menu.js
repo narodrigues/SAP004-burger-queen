1fd405111cc4d38a60c1d9374c7e0fb9225eedb6
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-07-25 18:48:12 -0300

    opções de menu caso da escolha do garçom

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,65 +1,113 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Img from '../imagem/Img';
 import Button from '../button/Button'
-// import optionBurger from '../optionMenu/option'
 import './menu.css'
 import firebase from '../../configure-firebase'
 
-const breakfast = (e) => {
-  e.preventDefault()
-  firebase
-    .firestore()
-    .collection('breakfast')
-    .get()
-    .then(querySnapshot => {
-      querySnapshot.forEach(doc => console.log(doc.data()))
-    })
-}
+const Menu = () => {
+  const [menuAllDay, setMenuAllDay] = useState(null);
+  const [menuBreakfast, setMenuBreakfast] = useState(null);
+  const [currentMenu, setCurrentMenu] = useState('allDay');
 
-const allDayArray = []; 
-const allDay = () => {
-  firebase
-    .firestore()
-    .collection('allday')
-    .get()
-    .then(querySnapshot => {
-      querySnapshot.forEach(doc => allDayArray.push(doc.data()));
-      // return allDayArray
-      // allDayArray.forEach(categories => categories.burger.forEach(burgers => console.log(burgers.name)))
-    })
-}
+  const breakfast = (e) => {
+    e.preventDefault()
+    firebase
+      .firestore()
+      .collection('breakfast')
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => { setMenuBreakfast(doc.data()) });
+      });
+  };
 
-const Menu = () => (
-  <section className='menu'>
-    <div className='div-menu'>
-      <div className='buttons-options-menu'>
-        <Button name='Matinal' className='option-menu-food' handleClick={breakfast} />
-        <Button name='Almoço/Janta' className='option-menu-food' handleClick={allDay} />
-      </div>
-      <div className='menu-principal bg-color'>
-        <div className='border-menu bg-color'>
-          {allDayArray.forEach(item => (
-            <div className='divs-option-menu'>
-              <div className='only-option-menu'>
-                <Img alt={item.burger.alt} className='img-food' />
-                <p>{item.burger.name}</p>
-                <p>{item.burger.price}</p>
+  const allDay = (e) => {
+    e.preventDefault();
+    setCurrentMenu('allDay')
+    firebase
+      .firestore()
+      .collection('allday')
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => { setMenuAllDay(doc.data()) });
+      });
+  };
+
+  return (
+    <section className='menu'>
+      <div className='div-menu'>
+        <div className='buttons-options-menu'>
+          <Button name='Matinal' className='option-menu-food' handleClick={breakfast} />
+          <Button name='Almoço/Janta' className='option-menu-food' handleClick={allDay} />
+        </div>
+        <div className='menu-principal bg-color'>
+          <div className='border-menu bg-color'>
+            {currentMenu === 'allDay' &&
+              <div>
+                {menuAllDay && menuAllDay.burger.map(item => (
+                  <div className='divs-option-menu' key={item.name}>
+                    <div className='only-option-menu'>
+                      <Img alt={item.alt} className='img-food' />
+                      <p>{item.name}</p>
+                      <p>{item.price}</p>
+                    </div>
+                  </div>
+                ))}
+                {menuAllDay && menuAllDay.startes.map(item => (
+                  <div className='divs-option-menu' key={item.name}>
+                    <div className='only-option-menu'>
+                      <Img alt={item.alt} className='img-food' />
+                      <p>{item.name}</p>
+                      <p>{item.price}</p>
+                    </div>
+                  </div>
+                ))}
+                {menuAllDay && menuAllDay.drinks.map(item => (
+                  <div className='divs-option-menu' key={item.name}>
+                    <div className='only-option-menu'>
+                      <Img alt={item.alt} className='img-food' />
+                      <p>{item.name}</p>
+                      <p>{item.price}</p>
+                    </div>
+                  </div>
+                ))}
               </div>
-            </div>
-          ))}
+            }
+            {currentMenu === 'breakfast' &&
+              <div>
+                {menuBreakfast && menuBreakfast.grilled.map(item => (
+                  <div className='divs-option-menu' key={item.name}>
+                    <div className='only-option-menu'>
+                      <Img alt={item.alt} className='img-food' />
+                      <p>{item.name}</p>
+                      <p>{item.price}</p>
+                    </div>
+                  </div>
+                ))}
+                {menuBreakfast && menuBreakfast.drinks.map(item => (
+                  <div className='divs-option-menu' key={item.name}>
+                    <div className='only-option-menu'>
+                      <Img alt={item.alt} className='img-food' />
+                      <p>{item.name}</p>
+                      <p>{item.price}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            }
+          </div>
         </div>
       </div>
-    </div>
-    <div className='requests bg-color'>
-      <div className='requests-quantity'>
-        <p>PEDIDOS</p>
+      <div className='requests bg-color'>
+        <div className='requests-quantity'>
+          <p>PEDIDOS</p>
+        </div>
+        <div className='total'>
+          <span>Total: </span>
+        </div>
+        <Button name='PEDIR' />
       </div>
-      <div className='total'>
-        <span>Total: </span>
-      </div>
-      <Button name='PEDIR' />
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default Menu;
--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import CloseModal from '../../components/modalCloseButton/ModalCloseButton';
+// import './modal.css'
 
 const Modal = (props) => {
-  const showHideClassName = props.show ? "modal display-block" : "modal display-none";
+  const [value, setValue] = useState(props.show)
+  const showHideClassName = value ? "modal display-block" : "modal display-none";
+
+  useEffect(() => {
+    setValue(props.show)
+  }, [props.show])
 
   return (
     <div className={showHideClassName}>
+      <CloseModal name='X' handleClick={() => { setValue(false) }} />
       <section className="modal-main">
         {props.children}
       </section>
@@ -13,3 +21,23 @@ const Modal = (props) => {
 };
 
 export default Modal;
+
+// const[cafe, setCafe] = useState(false)
+// const[almoço, setAlmoço] = useState(false)
+
+// useEffect(() => {
+//   {cafe && (
+//     breakfast()
+//   )},
+//   {almoço && (
+//     allDay()
+//   )}
+// }, [cafe, almoço])
+
+
+// const[cafe, setCafe] = useState(false)
+// const[almoço, setAlmoço] = useState(false)
+
+// const renderizo = cafe ? 'display: block' : 'display: none'
+// const renderizo2 = almoço ? 'display: block' : 'display: none'
+
