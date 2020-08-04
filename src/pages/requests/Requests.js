3296db51ebf9bb21f3a7141f2efef9b37ac0aeb1
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-08-04 14:40:46 -0300

    mudando nomes para as cork diferentes

--- src/pages/requests/Requests.js
+++ src/pages/requests/Requests.js
@@ -6,10 +6,10 @@ import firebase from '../../configure-firebase';
 import './requests.css'
 
 export default function Requests() {
-	const [readyOrder, setReadyOrder] = useState([]);
-	const [completedOrder, setCompletedOrder] = useState([]);
+  const [readyOrder, setReadyOrder] = useState([]);
+  const [completedOrder, setCompletedOrder] = useState([]);
 
-	useEffect(() => {
+  useEffect(() => {
     firebase
       .firestore()
       .collection('orders')
@@ -34,46 +34,46 @@ export default function Requests() {
       });
 
     const filter = readyOrder.filter(orders => orders !== item);
-		setCompletedOrder([...completedOrder, item]);
-		setReadyOrder([...filter]);
-	}
+    setCompletedOrder([...completedOrder, item]);
+    setReadyOrder([...filter]);
+  }
 
-	return (
-		<>
-			<section className='page-requests'>
-				<Header />
-			</section>
+  return (
+    <>
+      <section className='page-requests'>
+        <Header />
+      </section>
 
-			<Cork 
-				children={readyOrder &&
-					readyOrder.map(item => (
-						<div className='divs-orders' key={item.id}>
-							<p>Cliente: {item.client}</p>
-							<p>Mesa: {item.table}</p>
-							<p className='status-ready'>{item.status}</p>
-							{item.order.map(pedido =>
-								<p className='p-orders'>• {pedido.name}</p>
-							)}
-							<div>
-								<Button name='ENTREGUE' handleClick={() => changeStatus(item)} />
-							</div>
-						</div>
-					))
-				}
+      <Cork name='ENTREGUES'
+        children={readyOrder &&
+          readyOrder.map(item => (
+            <div className='divs-orders' key={item.id}>
+              <p>Cliente: {item.client}</p>
+              <p>Mesa: {item.table}</p>
+              <p className='status-ready'>{item.status}</p>
+              {item.order.map(pedido =>
+                <p className='p-orders'>• {pedido.name}</p>
+              )}
+              <div>
+                <Button name='ENTREGUE' handleClick={() => changeStatus(item)} />
+              </div>
+            </div>
+          ))
+        }
 
-				secondChildren={completedOrder &&
-					completedOrder.map(item => (
-						<div className='divs-orders' key={item.id}>
-							<p>Mesa: {item.table}</p>
-							<p>Cliente: {item.client}</p>
-							<p className='status-completed'>{item.status}</p>
-							{item.order.map(pedido =>
-								<p className='p-orders'>•{pedido.count}x {pedido.name}</p>
-							)}
-						</div>
-					))
-				}
-			/>
-		</>
-	)
+        secondChildren={completedOrder &&
+          completedOrder.map(item => (
+            <div className='divs-orders' key={item.id}>
+              <p>Mesa: {item.table}</p>
+              <p>Cliente: {item.client}</p>
+              <p className='status-completed'>{item.status}</p>
+              {item.order.map(pedido =>
+                <p className='p-orders'>•{pedido.count} x {pedido.name}</p>
+              )}
+            </div>
+          ))
+        }
+      />
+    </>
+  )
 }  
