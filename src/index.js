473fb5a0e7fd817221a67654fc9b3376dbf3072b
natasha <natasha.gr.costa@gmail.com>
2020-08-12 13:37:38 -0300

    refatoração

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from "react-router-dom";
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
